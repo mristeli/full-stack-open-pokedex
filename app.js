f14f96e5412064f8527456c2b408c38586e366e0
Matti Risteli <matti.risteli@iki.fi>
2022-02-13 10:47:38 +0200

    11.11. test failing heroku dep

--- app.js
+++ app.js
@@ -10,3 +10,11 @@ app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
 })
+
+app.get('/health', (_req, res) =>  {
+  res.send('ok')
+})
+
+app.get('/version', (_req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
